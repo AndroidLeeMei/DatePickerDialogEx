be58eefafb0e81f0f01f2dc8a3f6e4c86470de67
LeeMei <boubouian@yahoo.com.tw>
2017-12-26 10:38:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/auser/datepickerdialogex/DatePickerDialogEx.java
@@ -0,0 +1,13 @@
+package com.example.auser.datepickerdialogex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DatePickerDialogEx extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datepickerdialog_ex);
+    }
+}
